e3228ebcb0a31cd498fae3220ee1608c774b082d
Raunak Kedia <raunakkedia1@gmail.com>
2020-09-03 01:32:35 +0530

    Updates

---
+++ precache-manifest.32667827292df4232185eac7bf8d3682.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9416f8a0c4be0c2be39d4a5ae6afc369",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "f6dbdc2f6ad7f163e6bd",
+    "url": "/movie-search/static/css/main.98e349ee.chunk.css"
+  },
+  {
+    "revision": "f01788aa223b5c2ae399",
+    "url": "/movie-search/static/js/2.0b27f412.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.0b27f412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6dbdc2f6ad7f163e6bd",
+    "url": "/movie-search/static/js/main.9c4a66c4.chunk.js"
+  },
+  {
+    "revision": "e378c4ba723eddf83731",
+    "url": "/movie-search/static/js/runtime-main.9ec9dec6.js"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/movie-search/static/media/logo.02a9430b.svg"
+  }
+]);
